842204a6e37bb2e06394ad38e974176419fd0b2d
mrwilbroad <wfrancis169@gmail.com>
2023-12-09 06:54:39 +0300

    Bar graphs

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,12 @@ function App() {
       <Routes>
 
         <Route path="/react-data-visualizaion" Component={HomeComponent}/>
-        
+
         <Route path="/" Component={HomeComponent}/>
 
+
         <Route path="/graphs" Component={AGraphsVisulization}>
-          <Route path="chart" Component={FirstChart}/>
+          <Route path="/chart" Component={FirstChart}/>
           <Route path="bar-chart" Component={BarChartGraph} />
         </Route>
       </Routes>
